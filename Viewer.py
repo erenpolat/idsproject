35bd8edb36309e155b1f5e02ece704f9298a8794
erenpolat <63498628+erenpolat@users.noreply.github.com>
2022-04-05 22:58:52 +0300

    .

--- Viewer.py
+++ Viewer.py
@@ -2,33 +2,54 @@ import cv2
 import zmq
 import base64
 import numpy as np
+import tkinter
 
-context = zmq.Context()
-footage_socket = context.socket(zmq.SUB)
-footage_socket2 = context.socket(zmq.PUB)
-footage_socket2.connect('tcp://172.20.10.7:5555')
-footage_socket.bind('tcp://*:5555')
-footage_socket.setsockopt_string(zmq.SUBSCRIBE, np.unicode(''))
-camera = cv2.VideoCapture(0)
+root = tkinter.Tk()
+root.geometry("200x90")
 
-while True:
-    try:
-        grabbed, myframe = camera.read()  # grab the current frame
-        myframe = cv2.resize(myframe, (640, 480))  # resize the frame
-        encoded, buffer = cv2.imencode('.jpg', myframe)
-        jpg_as_text = base64.b64encode(buffer)
-        footage_socket2.send(jpg_as_text)
+text = tkinter.Text(root, height = 1, width = 22)
 
-        frame = footage_socket.recv_string()
-        img = base64.b64decode(frame)
-        npimg = np.fromstring(img, dtype=np.uint8)
-        source = cv2.imdecode(npimg, 1)
+def start():
+    ip = "tcp://" + text.get("1.0", "end-1c") + ":5555"
+    print(ip)
+    context = zmq.Context()
+    footage_socket = context.socket(zmq.SUB)
+    footage_socket2 = context.socket(zmq.PUB)
+    footage_socket2.connect(ip)
+    footage_socket.bind('tcp://*:5555')
+    footage_socket.setsockopt_string(zmq.SUBSCRIBE, np.unicode(''))
+    camera = cv2.VideoCapture(0)
 
-        horizontal_image = cv2.hconcat([myframe, source])
+    root.destroy()
 
-        cv2.imshow("Stream", horizontal_image)
-        cv2.waitKey(1)
+    while True:
+        try:
+            grabbed, myframe = camera.read()  # grab the current frame
+            myframe = cv2.resize(myframe, (640, 480))  # resize the frame
+            encoded, buffer = cv2.imencode('.jpg', myframe)
+            jpg_as_text = base64.b64encode(buffer)
+            footage_socket2.send(jpg_as_text)
 
-    except KeyboardInterrupt:
-        cv2.destroyAllWindows()
-        break
+            frame = footage_socket.recv_string()
+            img = base64.b64decode(frame)
+            npimg = np.fromstring(img, dtype=np.uint8)
+            source = cv2.imdecode(npimg, 1)
+
+            horizontal_image = cv2.hconcat([myframe, source])
+            cv2.imshow("Stream", horizontal_image)
+
+            key = cv2.waitKey(1)
+
+            if key == 27:
+                break
+
+        except KeyboardInterrupt:
+            cv2.destroyAllWindows()
+            break
+
+button = tkinter.Button(root, text ="Submit", command=start)
+button.config(width=20, height=2)
+
+text.pack()
+button.pack()
+root.mainloop()
