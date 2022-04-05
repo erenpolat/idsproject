e482c59f2b5baabac93c8149ece2d7a36d369bc6
Fahrican Elidemir <fahricanelidemir@gmail.com>
2022-04-05 19:20:32 +0200

    Git init

---
+++ Broadcast.py
@@ -0,0 +1,12 @@
+import ipaddress
+
+IP = '172.20.10.2'
+MASK = '255.255.255.240'
+
+host = ipaddress.IPv4Address(IP)
+net = ipaddress.IPv4Network(IP + '/' + MASK, False)
+print('IP:', IP)
+print('Mask:', MASK)
+print('Subnet:', ipaddress.IPv4Address(int(host) & int(net.netmask)))
+print('Host:', ipaddress.IPv4Address(int(host) & int(net.hostmask)))
+print('Broadcast:', net.broadcast_address)
